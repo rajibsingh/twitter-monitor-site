757c376738848307c241b52229472d82f62b4887
Rajib Singh <rajibsingh@yahoo.com>
2016-11-23 12:48:28 -0500

    added mongodb driver & can connect to local mongo

--- server.js
+++ server.js
@@ -12,5 +12,19 @@ app.listen(3000, function() {
 app.get('/', function (req, res) {
     // console.log(__dirname)
     // response.sendFile(__dirname + '/index.html')
+
+    var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+
+    // Connection URL
+    var url = 'mongodb://localhost:27017/tweetsdb';
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        db.close();
+    });
+
     res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
