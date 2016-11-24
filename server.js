a9acc035ccfcb9e4c54cddd962aa95a232505bf0
Rajib Singh <rajibsingh@yahoo.com>
2016-11-23 19:54:27 -0500

    much fiddling around, settled on express, pug (aka jade) and just trying to wrap my head around promises

--- server.js
+++ server.js
@@ -4,27 +4,25 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug')
-
+app.set('views', __dirname + '/public/views');
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
 app.get('/', function (req, res) {
-    // console.log(__dirname)
-    // response.sendFile(__dirname + '/index.html')
-
-    var MongoClient = require('mongodb').MongoClient, assert = require('assert');
-
-    // Connection URL
+    var MongoClient = require('mongodb').MongoClient , assert = require('assert');
+    var tweets = [];
     var url = 'mongodb://localhost:27017/tweetsdb';
-
-    // Use connect method to connect to the Server
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
-
-        db.close();
+        var col = db.collection('tweets').find().toArray(function(err, results) {
+            console.log(results);
+            for (tweet in results) {
+                tweets.push(tweet);
+            }
+            db.close();
+            res.render('index', { title: 'Hey', message: 'Hello there!', tweets: tweets })
+        });
     });
-
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
