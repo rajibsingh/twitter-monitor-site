53df68fde4f8d2c545bcf626ef00460ca4483ea8
Rajib Singh <rajibsingh@yahoo.com>
2016-11-27 00:04:40 -0500

    detail view gets data, more than likely now need to show it

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-console.log('May Node be with you')
-
 const express = require('express');
 const app = express();
 
@@ -17,7 +15,6 @@ app.get('/', function (req, res) {
     var url = 'mongodb://localhost:27017/tweetsdb';
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
-        console.log("Connected correctly to server");
         var col = db.collection('tweets').find().toArray(function(err, results) {
             console.log(results);
             // ytf do I have to use an index-based for? was using a construct of
@@ -32,4 +29,27 @@ app.get('/', function (req, res) {
             res.render('index', { title: 'Hey', message: 'Hello there!', tweets: tweets })
         });
     });
+})
+
+app.get('/detail/:tweetId', function (req, res) {
+    var tweetId = req.params.tweetId;
+    console.log("tweetId: " + tweetId);
+
+    var MongoClient = require('mongodb').MongoClient , assert = require('assert');
+    var tweets = [];
+    var url = 'mongodb://localhost:27017/tweetsdb';
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        var analysis = "initialized but not set";
+        var tweet = "initialized but not set";
+        var analysisPromise = db.collection('processed_tweets').findOne({"_id":tweetId});
+        console.log("analysisPromise: " + analysisPromise);
+        analysis = analysisPromise.then(function (err, val) {
+            db.close();
+            res.render('detail', {"analysisPromise" : analysis, 'tweet': 'placeholder tweet'});
+        })
+
+    });
+
 })
