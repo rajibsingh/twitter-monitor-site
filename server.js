5cad5df91fe12dbf0489125b3e185dd3d7df1861
Rajib Singh <rajibsingh@yahoo.com>
2016-11-28 07:19:25 -0500

    working on detail view

--- server.js
+++ server.js
@@ -16,13 +16,10 @@ app.get('/', function (req, res) {
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
         var col = db.collection('tweets').find().toArray(function(err, results) {
-            console.log(results);
             // ytf do I have to use an index-based for? was using a construct of
-            //     for (tweet in tweets)
-            // but tweet was just an int of the item's index in the array
+            // for (tweet in tweets) but tweet was just an int of the item's index in the array
             for (i = 0; i < results.length; i++) {
                 tweet = results[i];
-                console.log("tweet: " + tweet);
                 tweets.push(tweet);
             }
             db.close();
@@ -36,19 +33,21 @@ app.get('/detail/:tweetId', function (req, res) {
     console.log("tweetId: " + tweetId);
 
     var MongoClient = require('mongodb').MongoClient , assert = require('assert');
-    var tweets = [];
     var url = 'mongodb://localhost:27017/tweetsdb';
 
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
-        var analysis = "initialized but not set";
+        console.log(err);
         var tweet = "initialized but not set";
-        var analysisPromise = db.collection('processed_tweets').findOne({"_id":tweetId});
+        var analysisPromise = db.collection('processed_tweets').findOne({"_id" : tweetId});
         console.log("analysisPromise: " + analysisPromise);
-        analysis = analysisPromise.then(function (err, val) {
-            db.close();
-            res.render('detail', {"analysisPromise" : analysis, 'tweet': 'placeholder tweet'});
-        })
+        analysisPromise.then(function (result) {
+                console.log("result: " + result);
+                db.close();
+                res.render('detail', {"analysis" : result, 'tweet': 'placeholder tweet'});
+        }, function(err) {
+            console.log(("err: ") + err);
+        });
 
     });
 
